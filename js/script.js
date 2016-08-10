775688646f494117b6b1572c80d1d9c5f877443d
cbilliau <christopher@billiau.com>
2016-08-10 08:35:54 -0400

    Added male/female choice option for pronouns.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,4 @@
 angular.module('textApp', [])
-	.controller('myCtrl', function($scope) {
-		var myForm = {
-			femaleName: "",
-			jobTitle: "",
-			tediousTask: "",
-			dirtyTask: "",
-			celebrity: "",
-			uselessSkill: "",
-			adjective: "",
-			obnoxiousCelebrity: "",
-			hugeNumber: ""
-		};
-		$scope.myForm = myForm;
+	.controller('RootCtrl', function() {
+		this.msg='hello';
 	});
