af633eda34384884e0122b39c5233327863fa458
cbilliau <christopher@billiau.com>
2016-08-11 14:19:45 -0400

    Successfully added ng-submit, ng-show, ng-hide. App meets refactor 3 requirements.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 angular.module('textApp', [ 'ngMessages' ])
 	.controller('RootCtrl', [ '$scope', function($scope) {
+		//Example of tieing data to script
 		this.msg='Automate This!';
+		$scope.showResults = false;
+		$scope.showForm = true;
 
+		//Eval data submitted from madForm
+		$scope.submit = function(){
+			if( $scope.madForm.$valid ) {
+			$scope.showResults = true;
+			} else {
+			$scope.showResults = false;
+			}
+		}
 	}]);
