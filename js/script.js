1bb9f37fa4077ef29ec6c0912e9afedfb816b7ba
cbilliau <christopher@billiau.com>
2016-08-13 21:38:27 -0400

    Added reset form function when Start Over clicked.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
-angular.module('textApp', [ 'ngMessages' ])
-	.controller('RootCtrl', [ '$scope', function($scope) {
-		//Example of tieing data to script
-		this.msg='Automate This!';
-		$scope.showResults = false;
-		$scope.showForm = true;
+angular.module('textApp', ['ngMessages', 'ngAnimate'])
+    .controller('RootCtrl', ['$scope', function($scope) {
+        //Example of tieing data to script
+        this.msg = 'Automate This!';
+        $scope.showResults = false;
+        $scope.showForm = true;
 
-		//Eval data submitted from madForm
-		$scope.submit = function(){
-			if( $scope.madForm.$valid ) {
-			$scope.showResults = true;
-			} else {
-			$scope.showResults = false;
-			}
-		}
-	}]);
+        //Eval data submitted from madForm
+        $scope.submit = function() {
+            if ($scope.madForm.$valid) {
+                $scope.showResults = true;
+            } else {
+                $scope.showResults = false;
+            }
+        };
+
+        // Reset madForm
+        $scope.reset = function() {
+            $scope.name = null;
+            $scope.jobTitle = null;
+            $scope.tediousTask = null;
+            $scope.dirtyTask = null;
+            $scope.celebrity = null;
+            $scope.uselessSkill = null;
+            $scope.adjective = null;
+            $scope.obnoxiousCelebrity = null;
+            $scope.hugeNumber = null;
+        };
+    }]);
